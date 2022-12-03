8df8eb12fc75296e45f94cde9dfc36099817dfed
Abrar(Desktop) <abrar.ahmed8875288788@gmail.com>
2022-12-03 18:10:05 +0530

    created a add_numbers function

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Creating a function to add two numbers
+"""
+
+def add_numbers(num1,num2):
+    result=num1+num2
+    return result
+
+
+final_result=add_numbers(12,13)
+
+print("Addition is",final_result)
